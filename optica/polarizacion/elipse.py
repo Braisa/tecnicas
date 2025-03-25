753e33745ae3f37c8cb1e27ac00388fb5125d31b
Braisa <oterolemabrais@gmail.com>
2025-03-25 12:19:40 +0100

    Optica - Polarizacion - Elipse

---
+++ optica/polarizacion/elipse.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as pch
+
+theta = np.radians(10 * np.arange(36))
+P = 1e-2 * np.array((81,86,90,89,81,82,73,64,52,43,35,31,31,34,40,49,58,68,
+                     82,92,97,94,94,85,75,62,50,41,33,29,28,33,41,50,60,70))
+a1, b1, o1 = .95, .6, 25
+
+fig, ax = plt.subplots()
+
+theta_lin = np.radians(np.linspace(0, 360, 100))
+
+ax.add_patch(pch.Ellipse((0,0),2*a1,2*b1,angle = o1, fill = False))
+ax.plot(np.sqrt(P)*np.cos(theta), np.sqrt(P)*np.sin(theta), "o", color = "tab:orange", label = "Medidas")
+
+ax.set_xlabel(r"$x$")
+ax.set_ylabel(r"$y$")
+
+fig.savefig("optica/polarizacion/elipse.pdf", dpi = 300, bbox_inches = "tight")
