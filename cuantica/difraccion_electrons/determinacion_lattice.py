28da17b47b0a742549311eb5e48d4794eebfb4aa
Braisa <oterolemabrais@gmail.com>
2025-04-14 16:36:30 +0200

    Cuantica - Difraccion electrons - Mejoras

--- cuantica/difraccion_electrons/determinacion_lattice.py
+++ cuantica/difraccion_electrons/determinacion_lattice.py
@@ -5,8 +5,7 @@ from scipy.optimize import curve_fit
 D1s = 1e-3 * np.array((29.845,27.650,25.870,24.915,22.550))
 D2s = 1e-3 * np.array((51.280,46.800,43.185,40.305,38.455))
 
-u_D_ind = 1e-3 * 0.1
-u_D = u_D_ind / np.sqrt(2)
+u_D = 1e-3 * 0.035
 
 Vs = 1e3 * np.array((3.0,3.5,4.0,4.5,5.0))
 
@@ -22,9 +21,9 @@ fig, ax = plt.subplots()
 xlin = np.linspace(.95*np.min(1/np.sqrt(Vs)), 1.05*np.max(1/np.sqrt(Vs)), 1000)
 
 ax.plot(xlin, 1e3 * recta(xlin, k1), ls = "dashed", color = "tab:orange", label = " ")
-ax.plot(xlin, 1e3 * recta(xlin, k2), ls = "dashed", color = "tab:green", label = " ")
-ax.plot(1/np.sqrt(Vs), 1e3 * D1s, "o", color = "tab:orange", label = r"$k_1$")
-ax.plot(1/np.sqrt(Vs), 1e3 * D2s, "^", color = "tab:green", label = r"$k_2$")
+ax.plot(xlin, 1e3 * recta(xlin, k2), ls = "dashed", color = "tab:blue", label = " ")
+ax.plot(1/np.sqrt(Vs), 1e3 * D1s, "o", color = "tab:orange", label = r"$b_1$")
+ax.plot(1/np.sqrt(Vs), 1e3 * D2s, "^", color = "tab:blue", label = r"$b_2$")
 
 ax.set_xlim(left = np.min(xlin), right = np.max(xlin))
 ax.legend(loc = "best", ncol = 2, handletextpad = 0.3, columnspacing = -0.6)
@@ -32,6 +31,12 @@ ax.legend(loc = "best", ncol = 2, handletextpad = 0.3, columnspacing = -0.6)
 ax.set_xlabel(r"$V_A^{-\dfrac{1}{2}}$ (V$^{-\dfrac{1}{2}}$)")
 ax.set_ylabel(r"$D$ (mm)")
 
+ax.xaxis.set_major_locator(plt.MultipleLocator(0.001))
+ax.xaxis.set_minor_locator(plt.MultipleLocator(0.0002))
+ax.yaxis.set_major_locator(plt.MultipleLocator(5))
+ax.yaxis.set_minor_locator(plt.MultipleLocator(1))
+
+fig.tight_layout()
 fig.savefig("cuantica/difraccion_electrons/determinacion_lattice.pdf", dpi = 300, bbox_inches = "tight")
 
 print(f"k1 = {k1}({u_k1})")
